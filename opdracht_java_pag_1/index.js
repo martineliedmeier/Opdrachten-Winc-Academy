ad107bcf56b3e19e3a351745adabd487fc86f3a6
martineliedmeier <86772368+martineliedmeier@users.noreply.github.com>
2021-07-27 16:17:50 +0200

    Add files via upload

---
+++ opdracht_java_pag_1/index.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy");
+let voorNaam = 'Martine';
+console.log (voorNaam);
+let appels = 4;
+let peren = 4;
+console.log(appels+peren);
+let druif = 13;
+let snoet = 6;
+console.log(druif-appels);
+console.log(snoet*peren);
+let leeftijd = 'zevenenveertig';
+console.log(typeof leeftijd);
+
