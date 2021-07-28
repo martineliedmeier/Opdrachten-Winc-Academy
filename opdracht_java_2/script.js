e4583b784a2fffddeb6b7691dd1d1d9438032a6f
martineliedmeier <86772368+martineliedmeier@users.noreply.github.com>
2021-07-28 13:28:18 +0200

    Add files via upload

---
+++ opdracht_java_2/script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy Studenten");
+// Dit is een grote som!
+let vermenigvuldigen = 1230941  * 1823794;
+console.log("Uitkomst vermenigvuldiging is", vermenigvuldigen);
+let deling = 637263 / 54;
+console.log("Antwoord deling is", deling);
+// Nee, de comment wordt niet opgepakt bij het runnen van de code - that's the point..
+/* Ook regels met programming opdrachten worden niet meegenomen, als je ze in een (block) comment zet. Handig als wilt testen waar er iets fout gaat in je coding. Handig om te weten: CTRL/ zet de line comment aan en uit. SHFT-ALT-A is de shortcut voor block comments*/
