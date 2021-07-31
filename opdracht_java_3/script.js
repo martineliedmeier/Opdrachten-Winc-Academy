9cbfc732b2e6fbb088a6fe43e31077ff85a5c5f0
martineliedmeier <86772368+martineliedmeier@users.noreply.github.com>
2021-07-31 12:31:01 +0200

    Add files via upload

---
+++ opdracht_java_3/script.js
@@ -0,0 +1,25 @@
+const age = 19;
+
+if (age == 18) {
+    console.log ("Je mag naar binnen!")
+} else if (age > 18) {
+    console.log ("Je mag naar binnen!")  
+} else {
+    console.log ("Sorry, je bent te jong..")
+}
+
+const isFemale = true;
+
+if (isFemale == true) {
+    console.log ("Welkom bij de ladiesnight")
+} else  {
+    console.log ("See you next time!")
+}
+
+const driverStatus = "bob"
+
+if (driverStatus == "bob") {
+    console.log ("Rij veilig!")
+} else {
+    console.log ("Ik bel een taxi voor je")
+}
