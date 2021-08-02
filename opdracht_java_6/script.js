703a529438a49d4eecad8d07828a9a05d44d582b
martineliedmeier <86772368+martineliedmeier@users.noreply.github.com>
2021-08-02 12:25:16 +0200

    Add files via upload

---
+++ opdracht_java_6/script.js
@@ -0,0 +1,19 @@
+const maakPizza = function () {
+    console.log ("Maak deeg");
+    console.log ("Gooi deeg in het rond");
+    console.log ("Smeer tomatensaus over deeg")
+}
+
+const maakSushi = function () {
+    console.log ("Open je browser");
+    console.log ("Zoek een goed restauarant");
+    console.log ("Bestel sushi")
+}
+// Je ziet nog niets als je alleen de functie defined hebt.
+
+maakSushi ();
+maakSushi ();
+maakSushi ();
+maakPizza ();
+maakPizza ();
+
